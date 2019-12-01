75aacf2d475911414435396b65038191d6a59b76
zhaoxinsheng <zhaoxinsheng@gmail.com>
2019-12-01 21:03:11 +0800

    add file

--- src/components/Charts/dairlyairout.vue
+++ src/components/Charts/dairlyairout.vue
@@ -7,6 +7,8 @@
   import resize from './mixins/dayair'
 
   export default {
+    dataList: [],
+    xdata: [],
     mixins: [resize],
     props: {
       className: {
@@ -28,7 +30,11 @@
     },
     data () {
       return {
-        chart: null
+        chart: null,
+        dataList: [],
+        xdata: [],
+        avgData: '',
+        totalData: ''
       }
     },
     mounted () {
@@ -43,155 +49,181 @@
     },
 
     methods: {
-      initChart () {
+      trasferAgvData(agvData){
+        this.$emit('transferAgvData',agvData);
+      },
+      initChart (date) {
         this.chart = echarts.init(document.getElementById(this.dayair))
-        const days = []
-        for (let i = 1; i <= 24; i++) {
-          days.push(i)
-        }
-        this.chart.setOption({
-          backgroundColor: 'white',
-          title: {
-            top: 20,
-            text: '日排气压力曲线',
-            textStyle: {
-              fontWeight: 'normal',
-              fontSize: 16,
-              color: 'black'
-            },
-            left: '1%'
-          },
-          tooltip: {
-            trigger: 'axis',
-            axisPointer: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            }
-          },
-          legend: {
-            top: 20,
-            icon: 'rect',
-            itemWidth: 14,
-            itemHeight: 5,
-            itemGap: 13,
-            data: ['排气压力值', '压力平均值'],
-            right: '4%',
-            textStyle: {
-              fontSize: 12,
-              color: 'black'
-            }
-          },
-          grid: {
-            top: 100,
-            left: '3%',
-            right: '4%',
-            bottom: '2%',
-            containLabel: true
-          },
-          xAxis: [{
-            type: 'category',
-            boundaryGap: false,
-            name: '时/h',
-            axisLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            },
-            data: days
-          }],
-          yAxis: [{
-            type: 'value',
-            name: 'P/压力值Mpa',
-            axisTick: {
-              show: false
-            },
-            axisLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            },
-            axisLabel: {
-              margin: 10,
-              textStyle: {
-                fontSize: 14
-              }
-            },
-            splitLine: {
-              lineStyle: {
-                color: '#57617B'
-              }
-            }
-          }],
-          series: [
-            {
-              name: '排气压力值',
-              type: 'line',
-              smooth: true,
-              symbol: 'circle',
-              symbolSize: 5,
-              showSymbol: false,
-              lineStyle: {
-                normal: {
-                  width: 1
-                }
-              },
-              areaStyle: {
-                normal: {
-                  color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                    offset: 0,
-                    color: 'rgba(0, 136, 212, 0.3)'
-                  }, {
-                    offset: 0.8,
-                    color: 'rgba(0, 136, 212, 0)'
-                  }], false),
-                  shadowColor: 'rgba(0, 0, 0, 0.1)',
-                  shadowBlur: 10
-                }
-              },
-              itemStyle: {
-                normal: {
-                  color: 'rgb(0,136,212)',
-                  borderColor: 'rgba(0,136,212,0.2)',
-                  borderWidth: 12
 
+        const days = [];
+        this.dataList = [];
+        this.xdata = [];
+        this.dataListLoading = true
+        this.$http({
+          url: this.$http.adornUrl(`/collect/line/press/1/` + date),
+          method: 'get',
+          params: this.$http.adornParams({
+            'type': '1'
+          })
+        }).then(({data}) => {
+
+
+          if (data && data.code === 0 && data.data != null) {
+            this.dataList = data.data.dataList
+            this.xdata = data.data.xDataList
+            this.avgData = data.data.avgData
+            this.totalData = data.data.totalData
+            for (let i = 0; i <= this.xdata.length; i++) {
+              days.push(this.xdata[i])
+            }
+            this.trasferAgvData(this.avgData);
+          }
+
+          this.dataListLoading = false;
+          this.chart.setOption({
+            backgroundColor: 'white',
+            title: {
+              top: 20,
+              text: '日排气压力曲线',
+              textStyle: {
+                fontWeight: 'normal',
+                fontSize: 16,
+                color: 'black'
+              },
+              left: '1%'
+            },
+            tooltip: {
+              trigger: 'axis',
+              axisPointer: {
+                lineStyle: {
+                  color: '#57617B'
+                }
+              }
+            },
+            // legend: {
+            //   // top: 20,
+            //   // icon: 'rect',
+            //   // itemWidth: 14,
+            //   // itemHeight: 5,
+            //   // itemGap: 13,
+            //   // data: ['排气压力值', '压力平均值'],
+            //   // right: '4%',
+            //   // textStyle: {
+            //   //   fontSize: 12,
+            //   //   color: 'black'
+            //   // }
+            // },
+            grid: {
+              top: 100,
+              left: '3%',
+              right: '4%',
+              bottom: '2%',
+              containLabel: true
+            },
+            xAxis: [{
+              type: 'category',
+              boundaryGap: false,
+              name: '时/h',
+              axisLine: {
+                lineStyle: {
+                  color: '#57617B'
                 }
               },
-              data: [0.82, 0.8, 0.9, 0.5, 0.74, 0.6, 0.4, 0.8, 0.9, 0.6, 0.4, 0.7,0.82, 0.8, 0.9, 0.5, 0.74, 0.6, 0.4, 0.8, 0.9, 0.6, 0.4, 0.7]
-            }, {
-              name: '压力平均值',
-              type: 'line',
-              smooth: true,
-              symbol: 'circle',
-              symbolSize: 5,
-              showSymbol: false,
-              lineStyle: {
-                normal: {
-                  width: 1
+              data: days
+            }],
+            yAxis: [{
+              type: 'value',
+              name: 'Bar',
+              axisTick: {
+                show: false
+              },
+              axisLine: {
+                lineStyle: {
+                  color: '#57617B'
                 }
               },
-              areaStyle: {
-                normal: {
-                  color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
-                    offset: 0,
-                    color: 'rgba(219, 50, 51, 0.3)'
-                  }, {
-                    offset: 0.8,
-                    color: 'rgba(219, 50, 51, 0)'
-                  }], false),
-                  shadowColor: 'rgba(0, 0, 0, 0.1)',
-                  shadowBlur: 10
+              axisLabel: {
+                margin: 10,
+                textStyle: {
+                  fontSize: 14
                 }
               },
-              itemStyle: {
-                normal: {
-                  color: 'rgb(219,50,51)',
-                  borderColor: 'rgba(219,50,51,0.2)',
-                  borderWidth: 12
+              splitLine: {
+                lineStyle: {
+                  color: '#57617B'
                 }
-              },
-              data: [0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74,0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74, 0.74]
-            }]
+              }
+            }],
+            series: [
+              {
+                name: '排气压力值',
+                type: 'line',
+                smooth: true,
+                symbol: 'circle',
+                symbolSize: 5,
+                showSymbol: false,
+                lineStyle: {
+                  normal: {
+                    width: 1
+                  }
+                },
+                areaStyle: {
+                  normal: {
+                    color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                      offset: 0,
+                      color: 'rgba(0, 136, 212, 0.3)'
+                    }, {
+                      offset: 0.8,
+                      color: 'rgba(0, 136, 212, 0)'
+                    }], false),
+                    shadowColor: 'rgba(0, 0, 0, 0.1)',
+                    shadowBlur: 10
+                  }
+                },
+                itemStyle: {
+                  normal: {
+                    color: 'rgb(0,136,212)',
+                    borderColor: 'rgba(0,136,212,0.2)',
+                    borderWidth: 12
+
+                  }
+                },
+                data: this.dataList
+              }, {
+                name: '压力平均值',
+                type: 'line',
+                smooth: true,
+                symbol: 'circle',
+                symbolSize: 5,
+                showSymbol: false,
+                lineStyle: {
+                  normal: {
+                    width: 1
+                  }
+                },
+                // areaStyle: {
+                //   normal: {
+                //     color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [{
+                //       offset: 0,
+                //       color: 'rgba(219, 50, 51, 0.3)'
+                //     }, {
+                //       offset: 0.8,
+                //       color: 'rgba(219, 50, 51, 0)'
+                //     }], false),
+                //     shadowColor: 'rgba(0, 0, 0, 0.1)',
+                //     shadowBlur: 10
+                //   }
+                // },
+                itemStyle: {
+                  normal: {
+                    color: 'rgb(219,50,51)',
+                    borderColor: 'rgba(219,50,51,0.2)',
+                    borderWidth: 12
+                  }
+                },
+                data: this.avgData
+              }]
+          })
         })
       }
     }
--- src/views/contents/power/dailyairoutpressreport.vue
+++ src/views/contents/power/dailyairoutpressreport.vue
@@ -5,15 +5,16 @@
         placeholder="选择日期"
         v-model="startTime" value-format="yyyy-MM-dd" format="yyyy-MM-dd">
       </el-date-picker>
-      <el-button type="primary">查询</el-button>
+
+      <el-button type="primary" @click="getData()">查询</el-button>
       <el-button type="primary" @click="getChartPdf">导出</el-button>
       <el-input
-        placeholder="请输入内容"
-        v-model="input1"
+        placeholder=""
+        v-model="avgData"
         :disabled="true" style="width: 30%;padding-left: 100px;color: red">
       </el-input>
     </div>
-    <dayairChart ref="dayairChart" height='100%' width='100%'></dayairChart>
+    <dayairChart ref="dayairChart" height='100%' width='100%' @transferAgvData="getAvgData"></dayairChart>
   </div>
 </template>
 
@@ -23,19 +24,41 @@
   export default {
     data () {
       return {
-        input1: '当日平均排气压力0.75Mpa',
+        avgData: '',
         startTime: new Date(),
         endTime: ''
       }
     },
     name: 'dayairChart1',
     components: { dayairChart },
-    
+
     methods: {
+      getAvgData(agvData) {
+          var avgstr = '平均排气压力 '+ agvData + ' Mpa';
+         this.avgData = avgstr;
+      },
+      getData() {
+        if (!this.startTime) {
+          this.$message('请选择需要查询的日期')
+          return false
+        }
+        var dateStr ;
+        if ('string' == typeof this.startTime) {
+           dateStr = this.startTime;
+        } else {
+          var month = this.prefixZero(this.startTime.getMonth(),2);
+          var day =  this.prefixZero(this.startTime.getDate(),2);
+          dateStr = this.startTime.getFullYear() + "-" + month  +"-" + day ;
+        }
+        this.$refs.dayairChart.initChart(dateStr);
+      },
       getChartPdf () {
         this.getPdf(this.$refs['dayairChart'].$el, '日气压力曲线')
+      },
+      prefixZero(num, n) {
+          return (Array(n).join(0) + num).slice(-n);
+        }
       }
-    }
   }
 </script>
 
